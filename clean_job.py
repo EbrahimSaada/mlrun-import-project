18d156849e94cb57e24c3bd71ac54dd54818e618
MichaelSkralivetsky <michael_skralivetsky@mckinsey.com>
2025-08-19 00:05:01 +0300

    add clean job for evidently snapshots

---
+++ clean_job.py
@@ -0,0 +1,22 @@
+def clean_function(context, path):
+    import os
+    import time
+    from pathlib import Path
+
+    base_root = Path()
+    now = time.time()
+    age_limit = 1 * 24 * 60 * 60
+
+    for uuid_dir in base_root.iterdir(path):
+        snapshots_dir = uuid_dir / "snapshots"
+        if snapshots_dir.is_dir():
+            for file in snapshots_dir.iterdir():
+                try:
+                    if file.is_file():
+                        mtime = file.stat().st_mtime
+                        if now - mtime > age_limit:
+                            file.unlink()
+                            print(f"Deleted: {file}")
+                except Exception as e:
+                    print(f"Error deleting {file}: {e}")
+
